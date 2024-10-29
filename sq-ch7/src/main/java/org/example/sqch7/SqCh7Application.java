c93a2fbbeb7d64cca7d9f2de7b3cda17750799ff
tenacy <tentenacy@gmail.com>
2024-10-30 02:50:11 +0900

    7장 실습 완료

---
+++ sq-ch7/src/main/java/org/example/sqch7/SqCh7Application.java
@@ -0,0 +1,13 @@
+package org.example.sqch7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqCh7Application.class, args);
+    }
+
+}
---
+++ sq-ch7/src/test/java/org/example/sqch7/SqCh7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqch7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
